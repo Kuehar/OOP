16b9db5c3627766e30f334d4ad261fe6cc088ae9
Kuehar <50407759+Kuehar@users.noreply.github.com>
2020-09-16 23:57:09 +0900

    Add files via upload

---
+++ SimpleLocation.java
@@ -0,0 +1,11 @@
+public class SimpleLocation{
+    public double distance(SimpleLocation other){
+        // Body not shown
+    }
+
+    /* This is not suited for usecase. We have to use overload.
+    public int distance(SimpleLocation other){
+
+    }
+    */
+} 
